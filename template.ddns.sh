1c8e2669ea376e3b89a52d33888e2ae878762773
douglin <douglas.lin@gmail.com>
2014-11-10 16:34:43 -0500

    Remove duplicate templates

--- template.ddns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-source ./openrc.sh -e
-python ddns.py
-nsupdate -y {key_name}:{secret_key} -v A_records
-nsupdate -y {key_name}:{secret_key} -v PTR_fixed
-nsupdate -y {key_name}:{secret_key} -v PTR_float
-rm A_records PTR_f*
